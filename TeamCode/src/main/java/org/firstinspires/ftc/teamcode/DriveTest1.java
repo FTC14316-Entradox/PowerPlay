b4bdb300b2f2d1859874d09eb3bc38f103fa5c7a
Akira Yoshiyama <luckyanchor2@gmail.com>
2022-10-14 17:55:38 -0700

    mvp teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest1.java
@@ -16,7 +16,16 @@ public class DriveTest1 extends ThreadOpMode {
     public DcMotor bl = null;
     public DcMotor br = null;
 
+    public DcMotor cascade  = null;
+
     public Servo gripper = null;
+    public Servo slide1 = null;
+    public Servo slide2 = null;
+    public double timetime = 0;
+    public int servosFixed = 0;
+
+    int sum = 2;
+    double speedAdj = 1;
 
     //Hardware initialization code
     @Override
@@ -32,11 +41,15 @@ public class DriveTest1 extends ThreadOpMode {
         bl  = hardwareMap.get(DcMotor.class, "backleft");
         br  = hardwareMap.get(DcMotor.class, "backright");
 
+        cascade  = hardwareMap.get(DcMotor.class, "cascade");
+
         //Reversing power for left wheels
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
 
         gripper = hardwareMap.get(Servo.class, "gripper");
+        slide1 = hardwareMap.get(Servo.class, "wire1");
+        slide2 = hardwareMap.get(Servo.class, "wire2");
 
         registerThread(new TaskThread(new TaskThread.Actions() {
             @Override
@@ -49,24 +62,25 @@ public class DriveTest1 extends ThreadOpMode {
                 double xAdj = x * Math.abs(x) * Math.abs(x);
 
                 if (y == 0.0) {
-                    fl.setPower(xAdj);
-                    bl.setPower(-xAdj);
-                    fr.setPower(-xAdj);
-                    br.setPower(xAdj);
+                    fl.setPower(-xAdj*speedAdj);
+                    bl.setPower(xAdj*speedAdj);
+                    fr.setPower(xAdj*speedAdj);
+                    br.setPower(-xAdj*speedAdj);
                 }
 
                 if (Math.abs(x) < Math.abs(y * Math.tan(3.1415 / 6))) {
-                    fl.setPower(yAdj);
-                    bl.setPower(yAdj);
-                    fr.setPower(yAdj);
-                    br.setPower(yAdj);
+                    fl.setPower(yAdj*speedAdj);
+                    bl.setPower(yAdj*speedAdj);
+                    fr.setPower(yAdj*speedAdj);
+                    br.setPower(yAdj*speedAdj);
                 }
 
+                //Make this more sensitive and add buttons for fast turn otherwise
                 if (Math.abs(rx) > 0) {
-                    fl.setPower(-rx);
-                    bl.setPower(-rx);
-                    fr.setPower(rx);
-                    br.setPower(rx);
+                    fl.setPower(-rx*0.6);
+                    bl.setPower(-rx*0.6);
+                    fr.setPower(rx*0.6);
+                    br.setPower(rx*0.6);
                 }
             }
         }));
@@ -75,10 +89,75 @@ public class DriveTest1 extends ThreadOpMode {
             @Override
             public void loop() {
                 if (gamepad1.x) {
-                    gripper.setPosition(0.5);
+                    fl.setPower(-1);
+                    bl.setPower(-1);
+                    fr.setPower(1);
+                    br.setPower(1);
                 }
                 if (gamepad1.b) {
+                    fl.setPower(1);
+                    bl.setPower(1);
+                    fr.setPower(-1);
+                    br.setPower(-1);
+                }
+            }
+        }));
+
+        //Speed control
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            @Override
+            public void loop() {
+                if (gamepad1.y) {
+                    sum = sum + 1;
+                }
+                if (sum % 2 == 0) {
+                    speedAdj = 1;
+                }
+                if (sum % 2 == 1) {
+                    speedAdj = 0.3;
+                }
+            }
+        }));
+
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            @Override
+            public void loop() {
+                if (gamepad2.x) {
                     gripper.setPosition(0.5);
+                }
+                if (gamepad2.b) {
+                    gripper.setPosition(0.6);
+                }
+            }
+        }));
+
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            @Override
+            public void loop() {
+                while (gamepad2.y) {
+                    cascade.setPower(0.8);
+                }
+                while (gamepad2.a) {
+                    cascade.setPower(-0.8);
+                }
+                cascade.setPower(0);
+            }
+        }));
+
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            @Override
+            public void loop() {
+                if (servosFixed == 0) {
+                    slide1.setDirection(Servo.Direction.REVERSE);
+                    slide2.setDirection(Servo.Direction.FORWARD);
+
+                    slide1.setPosition(0.6);
+                    timetime = getRuntime();
+                    while ((timetime + 1) > getRuntime()) {
+                        telemetry.addData("Waiting", "1 second1");
+                    }
+                    slide2.setPosition(0.54);
+                    servosFixed = 1;
                 }
             }
         }));
